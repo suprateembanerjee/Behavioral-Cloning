92c3e28c1dcd4547ada743bed2c3d674998ba5fc
Suprateem Banerjee <dolphinextreme48@gmail.com>
2020-11-29 14:11:01 +0530

    Refactored code
    Added comments
    Removed unused imports
    Streamlined preprocessing

--- model.py
+++ model.py
@@ -4,31 +4,32 @@ import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 from time import time
 
-lines = []
-
 t1=time()
 
-with open('./data/driving_log.csv') as csvfile:
-	reader = csv.reader(csvfile)
-	for line in reader:
-		lines.append(line)
+# Read in image data and labels from all three cameras
 
 images = []
 measurements = []
 
-for line in lines:
-
-	steering_center = float(line[3])
-	correction = 0.2
-	steering_left = steering_center + correction
-	steering_right = steering_center - correction
-
-	image_center = mpimg.imread("./data/"+str(line[0]))
-	image_left = mpimg.imread("./data/"+str(line[1][1:]))
-	image_right = mpimg.imread("./data/"+str(line[2][1:]))
+with open('./data/driving_log.csv') as csvfile:
 	
-	images.extend((image_center, image_left, image_right))
-	measurements.extend((steering_center, steering_left, steering_right))
+	reader = csv.reader(csvfile)
+	
+	for line in reader:
+
+		# Augment labels for left and right cameras
+
+		steering_center = float(line[3])
+		correction = 0.2
+		steering_left = steering_center + correction
+		steering_right = steering_center - correction
+
+		image_center = mpimg.imread("./data/"+str(line[0]))
+		image_left = mpimg.imread("./data/"+str(line[1][1:]))
+		image_right = mpimg.imread("./data/"+str(line[2][1:]))
+		
+		images.extend((image_center, image_left, image_right))
+		measurements.extend((steering_center, steering_left, steering_right))
 
 X_train = np.array(images)
 y_train = np.array(measurements)
@@ -42,9 +43,6 @@ for image in X_train:
 X_train_flipped = np.array(X_train_flipped)
 y_train_flipped = np.multiply(y_train , -1.0)
 
-plt.imsave('flipped.png',X_train_flipped[0])
-plt.imsave('unflipped.png',X_train[0])
-
 X_train = np.append(X_train, X_train_flipped, axis = 0)
 y_train = np.append(y_train, y_train_flipped)
 
@@ -55,7 +53,7 @@ print("Time taken to preprocess: {:.2f}s".format(time() - t1))
 
 
 
-from tensorflow.keras.layers import Cropping2D, LayerNormalization, Input, Lambda, Dense, Flatten, Conv2D, Activation
+from tensorflow.keras.layers import Cropping2D, LayerNormalization, Input, Lambda, Dense, Flatten, Conv2D
 import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
 from tensorflow.keras.models import Model
@@ -67,10 +65,13 @@ from tensorflow.keras.optimizers.schedules import ExponentialDecay
 
 def nvidia():
 
-	from tensorflow.keras.layers.experimental.preprocessing import Normalization
+	"""
+	Implementation of Nvidia's End-to-End Learning model for Self-driving cars
+	"""
 
-	# NVIDIA
 	global X_train, y_train
+
+	# Model Design
 
 	inputs = Input(shape=(160,320,3))
 	cropped = Cropping2D(cropping=((64, 0), (0, 0)))(inputs)
@@ -87,6 +88,8 @@ def nvidia():
 	dense3 = Dense(10,activation='relu')(dense2)
 	out = Dense(1, activation='linear')(dense3)
 
+	# Specifications and training
+
 	checkpoint = ModelCheckpoint(filepath="./ckpts/model_nvidia.h5", monitor='val_loss', save_best_only=True)
 	stopper = EarlyStopping(monitor='val_loss', min_delta=0.0003, patience = 10)
 
@@ -96,11 +99,13 @@ def nvidia():
 	t2 = time()
 	model = Model(inputs=inputs, outputs=out)
 	model.compile(loss = loss, optimizer = optimizer)
-	history = model.fit(X_train, y_train, validation_split = 0.2, shuffle = True,
+	result = model.fit(X_train, y_train, validation_split = 0.2, shuffle = True,
 		epochs = 100, callbacks=[checkpoint, stopper])
 
-	plt.plot(history.history['loss'])
-	plt.plot(history.history['val_loss'])
+	# Visualization of loss variations across epochs
+
+	plt.plot(result.history['loss'])
+	plt.plot(result.history['val_loss'])
 	plt.title('Huber Loss')
 	plt.ylabel('Loss')
 	plt.xlabel('Epoch')
@@ -108,10 +113,9 @@ def nvidia():
 	plt.savefig('loss.png')
 	plt.show()
 
-	model.load_weights('./ckpts/model_nvidia.h5')
-
 	print("Time taken to train: {:.2f}s".format(time()-t2))
 
+	model.load_weights('./ckpts/model_nvidia.h5')
 	model.save('model.h5')
 
 
